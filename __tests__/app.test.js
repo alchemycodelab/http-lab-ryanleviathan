b3f7cab2aa04ad29c25841f4fafd97cb2469e026
ryanleviathan <ryan.m.carreras@gmail.com>
2020-12-14 18:44:15 -0800

    done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,4 +9,33 @@ describe('app routes', () => {
         expect(res.text).toEqual('hi');
       });
   });
+  it('responds with echo', async() => {
+    return await request(app)
+      .post('/echo')
+      .send('echo')
+      .then(res => {
+        expect(res.text).toEqual("echo");
+      });
+  });
+  it('responds with red', async() => {
+    return await request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual('<h1>red</h1>');
+      });
+  });
+  it('responds with green', async() => {
+    return await request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.text).toEqual('<h1>green</h1>');
+      });
+  });
+  it('responds with blue', async() => {
+    return await request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.text).toEqual('<h1>blue</h1>');
+      });
+  });
 });
